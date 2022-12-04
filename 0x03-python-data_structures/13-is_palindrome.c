58e9f30878c341a3fb96aa9133cd220a13a0e5b5
GentleGE <gentlege96@gmail.com>
2022-12-04 11:56:18 -0800

    Corrected Palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,68 +1,71 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * to_array - transform a linked lists into an array of nodes
- *
- * @head: head of the list
- *
- * @length: a pointer where the length of the array would
- *		be stored.
- *
- * Return: Array on node pointers or NULL if malloc failed.
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ * Return: pointer to the first node in the new list
  */
-int *to_array(listint_t *head, size_t *length)
+void reverse_listint(listint_t **head)
 {
-	int *array;
-	size_t arr_size = ARRSIZE, i = 0;
-	listint_t *temp = head;
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
 
-	if (!head)
-		return (NULL);
-	array = malloc(sizeof(int) * arr_size);
-	if (!array)
-		return (NULL);
-
-	while (temp)
+	while (current)
 	{
-		if (i == arr_size - 1)
-		{
-			arr_size += 5;
-			array = realloc(array, sizeof(int) * arr_size);
-			if (!array)
-				return (NULL);
-		}
-		array[i++] = temp->n;
-		temp = temp->next;
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
 	}
-	*length = i;
-	return (array);
+
+	*head = prev;
 }
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the linked list
  *
- * @head: the address of the head of the list.
- *
- * Return: 0 if the list is not a palindrome, 1 if it is a palindrome.
- *
- * NOTE: An empty list is considered a palindrome.
+ * Return: 1 if it is, 0 if not
  */
 int is_palindrome(listint_t **head)
 {
-	size_t len = 0;
-	int *arr, ind = 0, rnd;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-	arr = to_array(*head, &len);
-	if (!arr)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	rnd = len - 1;
-	while (ind < rnd)
-		if (arr[ind++] != arr[rnd--])
+
+	while (1)
+	{
+		fast = fast->next->next;
+		if (!fast)
 		{
-			free(arr);
-			return (0);
+			dup = slow->next;
+			break;
 		}
-	free(arr);
-	return (1);
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
+	}
+
+	reverse_listint(&dup);
+
+	while (dup && temp)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
+	}
+
+	if (!dup)
+		return (1);
+
+	return (0);
 }
